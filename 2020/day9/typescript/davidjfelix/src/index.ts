1ddffbfeb4ef3aab1a0cf834fb1ece05a0a11756
David J. Felix <felix.davidj@gmail.com>
2020-12-25 15:17:31 -0500

    feat: do day 9

---
+++ 2020/day9/typescript/davidjfelix/src/index.ts
@@ -0,0 +1,52 @@
+import fs from 'fs'
+import _, { max, min } from 'lodash'
+
+const input = fs
+  .readFileSync('./input.txt', 'utf8')
+  .split('\n')
+  .map((line) => parseInt(line))
+
+function extractPreambles(list: number[]): [number, number[]][] {
+  const codeList = list.slice(25)
+  const preambleList = list
+    .map((_, index, list) => list.slice(index, index + 25))
+    .slice(0, codeList.length)
+  return _.zip(codeList, preambleList) as [number, number[]][]
+}
+
+function generatePreambleSumSet(preamble: number[]): Set<number> {
+  return new Set(
+    preamble
+      .flatMap((left, i, list) =>
+        list.slice(i + 1).map((right) => [left, right]),
+      )
+      .map((pair) => pair.reduce((a, b) => a + b)),
+  )
+}
+
+const [incorrectValue] = extractPreambles(input)
+  .map(
+    ([val, preamble]) =>
+      [val, generatePreambleSumSet(preamble)] as [number, Set<number>],
+  )
+  .find(([val, preambleSet]) => !preambleSet.has(val)) as [number, Set<number>]
+
+// Part 1
+console.log(incorrectValue)
+
+function breakIncorrectValue(
+  input: number[],
+  incorrectValue: number,
+): number | undefined {
+  for (let first = 0; first < input.length - 1; first++) {
+    for (let second = first + 1; second < input.length; second++) {
+      let seq = input.slice(first, second)
+      let sum = seq.reduce((a, b) => a + b)
+      if (sum === incorrectValue) {
+        return min(seq)! + max(seq)!
+      }
+    }
+  }
+}
+
+console.log(breakIncorrectValue(input, incorrectValue))

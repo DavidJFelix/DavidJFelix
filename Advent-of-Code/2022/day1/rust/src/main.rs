b8d7c62bb98f56414f0ebe0f2d214dc120f4036e
David J. Felix <felix.davidj@gmail.com>
2022-12-01 12:40:00 -0600

    fix: don't drop out of iter

--- Advent-of-Code/2022/day1/rust/src/main.rs
+++ Advent-of-Code/2022/day1/rust/src/main.rs
@@ -3,7 +3,7 @@ use std::fs;
 
 fn main() {
     let contents: String = fs::read_to_string("../input.txt").unwrap();
-    let input: Vec<Vec<u64>> = contents
+    let input = contents
         .split("\n\n")
         // Remove the trailing newline group from the end
         .filter(|group| *group != "")
@@ -11,20 +11,19 @@ fn main() {
             group
                 .split("\n")
                 .map(|line| line.parse::<u64>().unwrap_or_default())
-                .collect()
-        })
-        .collect();
+        });
 
     let p1_max_calories: u64 = input
-        .iter()
-        .map(|calories| calories.iter().sum())
+        .to_owned()
+        .map(|calories| calories.sum())
         .max()
-        .unwrap();
+        .unwrap_or_default();
+
     print!("{:?}\n", p1_max_calories);
 
     let p2_top_3_calories_sum: u64 = input
-        .iter()
-        .map(|calories| calories.iter().sum::<u64>())
+        .to_owned()
+        .map(|calories| calories.sum::<u64>())
         .sorted()
         .rev()
         .take(3)

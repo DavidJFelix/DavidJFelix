f39c16958a4a95d5a6592d3ce90bda1196f0b577
David J. Felix <felix.davidj@gmail.com>
2024-12-01 14:10:57 -0600

    feat: excercise 5.2 toast screen reader

--- Joy-of-React/project-toast/src/components/ToastShelf/ToastShelf.js
+++ Joy-of-React/project-toast/src/components/ToastShelf/ToastShelf.js
@@ -8,7 +8,12 @@ function ToastShelf() {
   const {toasts, onCloseToast} = React.useContext(ToastContext);
 
   return (
-    <ol className={styles.wrapper}>
+    <ol
+      className={styles.wrapper}
+      role="region"
+      aria-live="polite"
+      aria-label="Notifications"
+      >
       {toasts.map(({id, variant, message}) => (
         <li key={`toast-${id}`} className={styles.toastWrapper}>
           <Toast variant={variant} onClose={()=>onCloseToast(id)}>{message}</Toast>

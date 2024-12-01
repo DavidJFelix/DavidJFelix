d9f7e87887ec73a4274ed742b2e06b32de9a94a6
David J. Felix <felix.davidj@gmail.com>
2024-12-01 14:04:51 -0600

    feat: excercise 5.1 keyboard use toasts

--- Joy-of-React/project-toast/src/components/ToastProvider/ToastProvider.js
+++ Joy-of-React/project-toast/src/components/ToastProvider/ToastProvider.js
@@ -24,6 +24,19 @@ function ToastProvider({children}) {
     });
   }, [setToasts]);
 
+  React.useEffect(() => {
+    function handleKeyDown(event) {
+      if (event.key === 'Escape') {
+        setToasts([]);
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown)
+    };
+  }, [setToasts]);
+
   return (
     <ToastContext.Provider value={{toasts, onCloseToast, popToast}}>
       {children}
